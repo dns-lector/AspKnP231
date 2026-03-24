a7f728c8cb3df368fef3bb0abb08fb640bbb8977
samoylenko_d <samoylenko_d@GREYROOM-50>
2026-03-24 09:14:35 +0200

    Saves

--- Data/Entities/ShopProduct.cs
+++ Data/Entities/ShopProduct.cs
@@ -1,6 +1,24 @@
-﻿namespace AspKnP231.Data.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspKnP231.Data.Entities
 {
     public class ShopProduct
     {
+        public Guid Id { get; set; }
+
+        public Guid ShopSectionId { get; set; }
+
+        public String Title { get; set; } = null!;
+
+        public String? Description { get; set; } = null!;
+
+        public String? Slug { get; set; } = null!;
+
+        public String? ImageUrl { get; set; } = null!;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
     }
 }
